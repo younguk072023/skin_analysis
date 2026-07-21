555170a0137129424b8e4aaf3bdce20f240069f5
younguk <younguk072023@gmail.com>
2026-07-21 14:06:48 +0900

    XGBOOST ADD

--- models/model_zoo.py
+++ models/model_zoo.py
@@ -4,32 +4,30 @@ from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier, Gradi
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
+from xgboost import XGBClassifier 
 
 def get_models(random_state=42):
+
     models = {
-        "RandomForest": RandomForestClassifier(
-            n_estimators=500,
-            random_state=random_state,
-            class_weight="balanced",
-            max_depth=None,
-            min_samples_leaf=2
-        ),
-
-        "ExtraTrees": ExtraTreesClassifier(
-            n_estimators=500,
-            random_state=random_state,
-            class_weight="balanced",
-            min_samples_leaf=2
-        ),
-
-        "GradientBoosting": GradientBoostingClassifier(
-            random_state=random_state
-        ),
 
         "LogisticRegression": Pipeline([
             ("scaler", StandardScaler()),
             ("clf", LogisticRegression(
                 max_iter=1000,
+                class_weight="balanced",
+                random_state=random_state
+            ))
+        ]),
+
+        "KNN": Pipeline([
+            ("scaler", StandardScaler()),
+            ("clf", KNeighborsClassifier(n_neighbors=5))
+        ]),
+
+        "SVM": Pipeline([
+            ("scaler", StandardScaler()),
+            ("clf", SVC(
+                kernel="linear",
                 class_weight="balanced",
                 random_state=random_state
             ))
@@ -44,10 +42,43 @@ def get_models(random_state=42):
             ))
         ]),
 
-        "KNN": Pipeline([
+        "RandomForest": Pipeline([
             ("scaler", StandardScaler()),
-            ("clf", KNeighborsClassifier(n_neighbors=5))
+            ("clf", RandomForestClassifier(
+            n_estimators=500,
+            random_state=random_state,
+            class_weight="balanced",
+            max_depth=None,
+            min_samples_leaf=2
+            ))
         ]),
+
+        "ExtraTrees": Pipeline([
+            ("scaler", StandardScaler()),
+            ("clf", ExtraTreesClassifier(
+            n_estimators=500,
+            random_state=random_state,
+            class_weight="balanced",
+            min_samples_leaf=2
+            ))
+        ]),
+
+        "GradientBoosting": Pipeline([
+            ("scaler", StandardScaler()),
+            ("clf", GradientBoostingClassifier(
+                random_state=random_state
+            ))
+        ]),
+
+        "XGBoost": Pipeline([
+            ("scaler", StandardScaler()),
+            ("clf", XGBClassifier(
+                n_estimators=500,
+                random_state=random_state,
+                eval_metric="logloss" 
+            ))
+        ])
+
     }
 
     return models
